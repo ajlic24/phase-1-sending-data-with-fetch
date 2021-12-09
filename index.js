dd9b25dcbf5edcf5de2a22a179140ce8ab7ddee7
ajlic24@gmail.com <ajlic24@gmail.com>
2021-12-08 20:45:16 -0500

    commit tree d9ea86d480f799f46a8e5c8b9c81adb8f9a59223

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const submitData = (name, email) => {
+    let userObj = {
+        name: `${name}`,
+        email: `${email}`
+    }
+    let body = document.querySelector(`body`)
+    let p = document.createElement(`p`)
+    return fetch(`http://localhost:3000/users`, {
+        method: `POST`,
+        headers: {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json'
+        },
+        body: JSON.stringify(userObj)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        p.textContent = data.id
+        body.appendChild(p)
+    })
+    .catch(data => {
+        p.textContent = data.message
+        body.appendChild(p)
+    })
+
+}
